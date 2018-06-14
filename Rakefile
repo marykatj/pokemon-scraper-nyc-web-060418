48ff473e22df429a4a394e0df35d0af5b2b50483
Mary-Kate <marykatj@gmail.com>
2018-06-13 21:32:55 -0400

    ready to push

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require_relative './lib/pokemon.rb'
+require 'pry'
+
+desc 'Loads up the console'
+task :console do
+  Pry.start
+end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
 class Pokemon
+
+  attr_accessor :type, :name, :id, :db
+
+  def initialize(props=[])
+    @name = props["name"]
+    @type = props["type"]
+    @id = props["id"]
+    @db = props["db"]
+
+  end
+
+  def self.save(name, type, db)
+      sql_string = <<-SQL_STRING
+        INSERT INTO pokemon(name, type) VALUES (?, ?)
+      SQL_STRING
+      db.execute(sql_string, name, type)
+  end
+
+  def self.find(id, db)
+      sql_string = <<-SQL_STRING
+        SELECT * FROM pokemon WHERE id= ?
+      SQL_STRING
+      pokemon_array = db.execute(sql_string, id)
+      pika = Pokemon.new(name: pokemon_array[0][1], type: pokemon_array[0][2], id: pokemon_array[0][0], db: db)
+binding.pry
+      # Pokemon.new(pokemon_hash)
+      # Pokemon.new(pokem)
+  end
+
 end
